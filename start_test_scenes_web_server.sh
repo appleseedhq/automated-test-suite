e90a3d6f4e88c2626ecdd6b8488c6c08db2ec31e
Kevin Masson <hi@oktomus.com>
2020-05-03 06:47:50 -0400

    Seperate current and previous job files

--- start_test_scenes_web_server.sh
+++ start_test_scenes_web_server.sh
@@ -7,8 +7,7 @@ docker container rm automated-test-suite-container
 
 docker run --name automated-test-suite-container \
 	--network host \
-	-v /home/johnny/automated-test-suite-www:/var/www/site:ro \
-	-v /home/johnny/build/sandbox/tests/test\ scenes:/var/www/test_scenes:ro \
+	-v /home/johnny/automated-test-suite-www:/var/www:ro \
 	-v /home/johnny/automated-test-suite/webserver_nginx_conf:/etc/nginx/conf.d:ro \
 	-p 8080:8080 \
  	-d nginx:alpine
